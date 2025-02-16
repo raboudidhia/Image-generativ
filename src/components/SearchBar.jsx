6243f6e74e3e90d1aa21a1f6b4538b7c4f3962f6
dhia_raboudi <mohameddhia.raboudi@etudiant-enit.utm.tn>
2025-02-16 20:40:41 +0100

    finish search bar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import styled from 'styled-components';
+import { SearchOutlined } from '@mui/icons-material';
+
+const SearchBarContainer = styled.div`
+
+ max-width: 550px;
+ display: flex;
+ width: max-content; /* Fixed typo from 'widht' to 'width' */
+ border : 1px solid ${({theme}) => theme.text_secondary + 90};
+color : ${({theme}) => theme.text_primary + 90};
+ border-radius: 8px;
+padding: 12px 16px;
+cursor: pointer;
+gap: 6px;
+align-items: center;
+width: 100%; /* Added width property */
+
+`;
+
+
+
+
+export const SearchBar = () => {
+  return (
+    <SearchBarContainer>
+        <SearchOutlined/>
+        <input
+         placeholder='Search with prompt or name ...'
+        style = {{
+            border : "none",
+            outline : "none",
+            width: "100%",
+            color: "inherit",
+            background: "transparent",
+        }} />
+    </SearchBarContainer>
+  )
+};
+export default SearchBar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,51 @@
+import { Search } from '@mui/icons-material';
 import styled from 'styled-components';
+import { SearchBar } from '../components/SearchBar';
 
 const Container = styled.div`
   height: 100%;
   overflow: scroll;
+  background: ${({ theme }) => theme.bg};
+  padding: 30px 30px;
+  padding-bottom: 50px;
+  display: flex;
+    flex-direction: column;
+    align-items: center;
+    @media (max-width: 768) {
+    padding: 6px 10px;
 `;
+
+const Headline = styled.div `
+  font-size: 34px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.text_primary};
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  @media (max-width: 600) {
+  fontsize: 22px;
+  
+  `;
+const Span = styled.div `
+ font-size: 30px;
+  font-weight: 800;
+  color: ${({ theme }) => theme.secondary};
+  
+  `;
 
 export const Home = () => {
   return (
-    <Container>Home</Container>
+    <Container>
+       <Headline>
+        Explore popular posts in the Community !
+        <Span>Generated with AI 🚀</Span>
+       </Headline>
+       <SearchBar />
+
+
+
+
+    </Container>
   );
 };
 
